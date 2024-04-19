28176495980440eacaadeefd8933be815738a533
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-04-19 09:04:41 +0300

    sending http request

--- src/main/java/core/HttpClient.java
+++ src/main/java/core/HttpClient.java
@@ -1,10 +1,20 @@
 package core;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class HttpClient {
+    //TODO: Make this async
     public void send(HttpRequest request) throws IOException {
-        Socket socket = new Socket(request.getAddress(), 80);
+        InetAddress address = request.getAddress();
+        Socket socket = new Socket(address, 80);
+
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        writer.println("GET /index.html HTTP/1.1\n" +
+                "Host: " + address.getHostName());
+
+
     }
 }
--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,9 +1,13 @@
 package core;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient();
         HttpRequest request = new HttpRequest("www.example.com");
+
+        client.send(request);
     }
 }
